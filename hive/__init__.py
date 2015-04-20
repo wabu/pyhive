211f3bc200b377f0dd6a511055bb3c520b1a7427
Daniel Waeber <daniel.waeber@gameduell.de>
2015-04-20 17:47:49 +0200

    first raw hdfs access

--- hive/__init__.py
+++ hive/__init__.py
@@ -1,6 +1,7 @@
 import asyncio
 import aiohs2
 import pandas as pd
+import subprocess
 
 import logging
 
@@ -66,6 +67,9 @@ class Framer:
     @coroutine
     def __call__(self, coro):
         raw = yield from coro
+        return self.mk_df(raw)
+
+    def mk_df(self, raw, na_val=None):
         if self.columns is None:
             if raw is None:
                 return None
@@ -75,9 +79,10 @@ class Framer:
                     self.warns.add('__schema__')
 
                 return pd.DataFrame(raw, dtype=object)
-
         df = pd.DataFrame(raw or None,  # self.empty,
                           columns=self.columns, dtype=object)
+        if na_val is not None:
+            df[df == na_val] = None
         df.index += self.offset
         self.offset += len(df)
 
@@ -94,6 +99,71 @@ class Framer:
                     logger.warning('Cannot convert %r to %r (%s)', col, typ, e)
                     self.warns.add(col)
         return df
+
+
+class RawHDFSChunker:
+    def __init__(self, hive, table, partitions):
+        self.hive = hive
+        self.table = table
+        self.partitions = partitions[:]
+
+        self.partition = None
+        self.framer = None
+        self.proc = None
+        self.tail = b''
+
+    @coroutine
+    def next_part(self):
+        yield from self.close()
+
+        self.partition = self.partitions.pop(0)
+        self.framer, self.proc = yield from self.hive._raw_hdfs(
+            self.table, self.partition)
+        self.tail = b''
+
+    @coroutine
+    def chunker(self):
+        while True:
+            if not self.partition:
+                yield from self.next_part()
+
+            chunk = yield from self.proc.stdout.read(24000000)
+            if not chunk:
+                self.partition = None
+                if self.tail:
+                    chunk = self.tail
+                    self.tail = b''
+                    break
+                else:
+                    continue
+            split = (self.tail + chunk).rsplit(b'\n', 1)
+            if len(split) == 1:
+                self.tail = chunk
+            else:
+                chunk, self.tail = split
+                break
+
+        if chunk:
+            raw = [l.split('\t')[1:] for l in chunk.decode().split('\n')]
+            return self.framer.mk_df(raw, na_val='')
+        else:
+            return None
+
+    def iter(self):
+        try:
+            while True:
+                fut = asyncio.async(self.chunker())
+                yield fut
+                if not fut.result:
+                    break
+        finally:
+            yield self.close()
+
+    @coroutine
+    def close(self):
+        if self.proc:
+            self.proc.send_signal(subprocess.signal.SIGINT)
+            yield from self.proc.wait()
 
 
 class AioHive:
@@ -124,6 +194,7 @@ class AioHive:
                 raise ValueError(
                     "could not find 'hive.metastore.uris' in config")
         self.cli = aiohs2.Client(host=host, port=port)
+        self.config = config
 
     @coroutine
     def execute(self, *rqs):
@@ -167,6 +238,52 @@ class AioHive:
     @coroutine
     def close(self):
         yield from self.cli.close()
+
+    @coroutine
+    def _raw_hdfs(self, table, partition):
+        info = (yield from self.fetch(
+            'describe formatted {table} partition ({partition})'
+            .format(table=table,
+                    partition=partition.replace('=', '="')+'"'))).applymap(
+                        str.strip)
+
+        i0, i1, *_ = pd.np.flatnonzero(info.col_name == '')
+        schema = info[i0+1:i1]
+        location = info.query('col_name == "Location:"').data_type.iloc[0]
+
+        columns = schema.col_name
+        dtypes = (schema.data_type
+                  .str.split('(').str[0].str.upper()
+                  .apply(hive_type_map.__getitem__))
+        framer = Framer(columns, dtypes)
+
+        proc = yield from asyncio.create_subprocess_exec(
+            'hadoop', 'fs', '-text',
+            '/'+location.split('://', 1)[1].split('/', 1)[1]+'/*',
+            stdout=subprocess.PIPE)
+
+        return framer, proc
+
+    @coroutine
+    def raw(self, table, **partitions):
+        parts = yield from self.fetch('show partitions {}'.format(table))
+        parts = parts.partition
+
+        info = parts.str.split('=')
+        names = info.str[0]
+        vals = info.str[1]
+
+        sel = pd.Series(False, index=parts.index)
+        for name, val in partitions.items():
+            if name not in names.values:
+                raise KeyError('no partition info {} in {}', name, table)
+            if isinstance(val, str):
+                val = [val]
+            for v in val:
+                sel |= (names == name) & (vals.str.contains(v))
+
+        rhc = RawHDFSChunker(self, table, list(parts[sel]))
+        return rhc.iter()
 
 
 class SyncedHive:
@@ -214,5 +331,18 @@ class SyncedHive:
             # ensure close is run
             self.run(it.throw(e))
             raise e
+
+    def raw(self, *args, **kws):
+        it = self.run(self.hive.raw(*args, **kws))
+        try:
+            for chunk in it:
+                data = self.run(chunk)
+                if data is not None and not data.empty:
+                    yield data
+        except BaseException as e:
+            # ensure close is run
+            self.run(it.throw(e))
+            raise e
+
 
 Hive = SyncedHive
